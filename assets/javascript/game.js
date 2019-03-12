b0dd0a206fd75920d7fb567f0ee14cf5e0529bd7
JagatheswariManoharan <jagatheswarimanoharan@Viswanathans-MacBook-Pro.local>
2019-03-11 22:53:04 -0700

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// Create array of characters
+const characters= ['captain-america','ironman','thanos','hulk'];
+let chr_hitCount = [20,40,30,60];
+let sel_character = undefined;
+let dyn_character = undefined;
+let sel_enemychr = undefined;
+let Echr_hitcount = undefined;
+// selecting your character
+function chr_selection(x){
+
+}
+// selecting the enemy
+function emy_selection(y){
+
+}
+// for attacking the enemies
+function on_attack(){
+
+}
+// to restart the game
+function retart(){
+
+}
+const init = _ => {
+
+}
+init()
